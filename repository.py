15570c3184ba5fd0d1dc9253930bcedcd4b83c36
teempe <tomasz.pe@interia.eu>
2020-11-23 20:21:19 +0100

    Update repository to save data in json file

--- repository.py
+++ repository.py
@@ -1,6 +1,27 @@
-def save(service, username, password):
-    """Save data to text file."""
+import json
 
-    entry_line = f"{service} | {username} | {password}\n"
-    with open("data/passwds.txt", "a") as file:
-        file.write(entry_line)
+
+DATA_FILE_PATH = "data/passwds.json"
+
+
+def save(service, username, password):
+    """Save data to json file."""
+
+    entry = {service : {
+        "username": username,
+        "password": password
+    }}
+
+    try:
+        with open(DATA_FILE_PATH, "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        write_json(entry)
+    else:
+        data.update(entry)
+        write_json(data)
+
+
+def write_json(data, filepath=DATA_FILE_PATH):
+    with open(filepath, "w") as data_file:
+        json.dump(data, data_file, indent=4)    
