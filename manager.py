47e66f217a32a7efdd6c0cd1fd3b8704f52495fd
teempe <tomasz.pe@interia.eu>
2020-11-23 17:35:10 +0100

    Add generate password

--- manager.py
+++ manager.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import repository
+import password
 
 
 WINDOW_BG = "#f4f4f2"
@@ -25,13 +26,18 @@ def save_data():
     password = password_entry.get()
 
     if is_entry_valid(service, user, password):
-        answer = messagebox.askyesno(service, f"User: {user}\nPassword: {password}\nIs it OK to save?"):
+        answer = messagebox.askyesno(service, f"User: {user}\nPassword: {password}\nIs it OK to save?")
         if answer:
             repository.save(service, user, password)
             clear_entries()
             messagebox.showinfo("Success", "Data have been saved.")
     else:
         messagebox.showerror("Error", "Please fill all fields.")
+
+
+def get_password():
+    new_password = password.generate_password()
+    password_entry.insert(0, new_password)
 
 
 root = Tk()
@@ -66,7 +72,7 @@ password_entry = Entry(root, width=25)
 password_entry.grid(row=3, column=1, sticky=W)
 
 # Buttons
-gen_btn = Button(root, text="Generate Password", bg=BUTTON_BG)
+gen_btn = Button(root, text="Generate Password", bg=BUTTON_BG, command=get_password)
 gen_btn.grid(row=3, column=2)
 
 add_btn = Button(root, text="Add", width=42, bg=BUTTON_BG, command=save_data)
---
+++ password.py
@@ -0,0 +1,38 @@
+import string
+import random
+
+
+def generate_password(n_letters=0, n_symbols=0, n_digits=0):
+    """
+    Generates random password.
+    Function takes required numbers of letters, symbols and digits and generates random passowrd.
+    If any of arguments is missing, it will be generated randomly.
+    
+    Paramaters:
+        n_letters (int): Required number of letters
+        n_digits (int): Required number of digits
+        n_symbols (int): Required number od symbols
+
+    Returns:
+        str: Generated password
+    """
+
+    n_letters = random.randint(8, 10) if n_letters == 0 else n_letters
+    n_symbols = random.randint(2, 4) if n_symbols == 0 else n_symbols
+    n_digits = random.randint(2, 4) if n_digits == 0 else n_digits
+
+    symbols=["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+    password = []
+    # selecting letters
+    password.extend([random.choice(string.ascii_letters) for _ in range(n_letters)])
+    # selecting symbols
+    password.extend([random.choice(symbols) for _ in range(n_symbols)])
+    # selecting digits
+    password.extend([random.choice(string.digits) for _ in range(n_digits)])
+    random.shuffle(password)
+    
+    return "".join(password)
+
+
+if __name__ == "__main__":
+    print(generate_password())
