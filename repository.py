8ac2c6eb57945cdbb5cdee87d33e99433e70911c
teempe <tomasz.pe@interia.eu>
2020-11-23 20:44:11 +0100

    Update repository to search for data

--- repository.py
+++ repository.py
@@ -4,6 +4,40 @@ import json
 DATA_FILE_PATH = "data/passwds.json"
 
 
+def read_json(filepath=DATA_FILE_PATH):
+    with open(filepath, "r") as data_file:
+        data = json.load(data_file)
+    return data    
+
+
+def write_json(data, filepath=DATA_FILE_PATH):
+    with open(filepath, "w") as data_file:
+        json.dump(data, data_file, indent=4) 
+
+
+def find_service(service_name):
+    """
+    Finds username and password for given service. If not found file or data returns None.
+
+    Paramaters:
+        service_name (str): Service name to look for
+
+    Returns:
+        tuple: (service, username, password) or None when nothing found
+    """
+    
+    try:
+        data = read_json()
+    except FileNotFoundError:
+        return None
+
+    data_found = data.get(service_name, None)
+    if data_found is not None:
+        return (service_name, data_found["username"], data_found["password"])
+    else:
+        return None
+
+
 def save(service, username, password):
     """Save data to json file."""
 
@@ -13,8 +47,7 @@ def save(service, username, password):
     }}
 
     try:
-        with open(DATA_FILE_PATH, "r") as data_file:
-            data = json.load(data_file)
+        data = read_json()
     except FileNotFoundError:
         write_json(entry)
     else:
@@ -22,6 +55,6 @@ def save(service, username, password):
         write_json(data)
 
 
-def write_json(data, filepath=DATA_FILE_PATH):
-    with open(filepath, "w") as data_file:
-        json.dump(data, data_file, indent=4)    
+if __name__ == "__main__":
+    print(find_service("Facebook"))
+    print(find_service("StackOverflow"))
